40e6eb47729074a97fa961a1a06695e9e03d3919
Jonathan Fuchs <jonathan.fuchs@gmail.com>
2012-10-09 22:06:43 -0700

    Gofmt'd the code.

--- grid.go
+++ grid.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-  "fmt"
-  //"io"
-  "log"
-  "net/http"
-  "regexp"
-  "container/list"
+	"container/list"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
 
-  "code.google.com/p/go.net/websocket"
+	"code.google.com/p/go.net/websocket"
 )
 
 const listenAddr = ":4000"
 
 func main() {
-  http.HandleFunc("/", rootHandler)
-  http.Handle("/socket", websocket.Handler(socketHandler))
-  err := http.ListenAndServe(listenAddr, nil)
-  if err != nil {
-    log.Fatal(err)
-  }
+	http.HandleFunc("/", rootHandler)
+	http.Handle("/socket", websocket.Handler(socketHandler))
+	err := http.ListenAndServe(listenAddr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-  http.ServeFile(w, r, "index.html")
+	http.ServeFile(w, r, "index.html")
 }
 
 // type subscriberQueue chan []byte
@@ -35,44 +34,43 @@ var clients = list.New()
 // var subscribers = make([]subscriber, 100)
 
 func socketHandler(ws *websocket.Conn) {
-  fmt.Printf("readWriteServer %#v\n", ws.Config())
+	fmt.Printf("readWriteServer %#v\n", ws.Config())
 
-  clientListEl := clients.PushBack(ws)
+	clientListEl := clients.PushBack(ws)
 
-  for {
-    buf := make([]byte, 100)
-    n, err := ws.Read(buf)
-    if err != nil {
-      fmt.Println(err)
-      break
-    }
-    go handleMessage(buf[:n], ws)
-  }
+	for {
+		buf := make([]byte, 100)
+		n, err := ws.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		go handleMessage(buf[:n], ws)
+	}
 
-  clients.Remove(clientListEl)
+	clients.Remove(clientListEl)
 
-  fmt.Println("readWriteServer finished")
+	fmt.Println("readWriteServer finished")
 }
 
 var updateRegExp, _ = regexp.Compile("^UPDATE\\:([0-9]+)\\,([0-9]+)\\:(.)$")
 
 func handleMessage(message []byte, origin *websocket.Conn) {
-    fmt.Printf("recv:%q\n", message)
+	fmt.Printf("recv:%q\n", message)
 
-    submatches := updateRegExp.FindSubmatch(message)
-    if submatches != nil {
-      fmt.Printf("x:    %q\n", submatches[1])
-      fmt.Printf("y:    %q\n", submatches[2])
-      fmt.Printf("char: %q\n", submatches[3])
-    }
+	submatches := updateRegExp.FindSubmatch(message)
+	if submatches != nil {
+		fmt.Printf("x:    %q\n", submatches[1])
+		fmt.Printf("y:    %q\n", submatches[2])
+		fmt.Printf("char: %q\n", submatches[3])
+	}
 
-    for e := clients.Front(); e != nil; e = e.Next() {
-      // do something with e.Value
-      conn := e.Value.(*websocket.Conn)
+	for e := clients.Front(); e != nil; e = e.Next() {
+		// do something with e.Value
+		conn := e.Value.(*websocket.Conn)
 
-      // if conn != origin {
-        websocket.Message.Send(conn, fmt.Sprintf("%s", message));
-      // }
-    }
+		// if conn != origin {
+		websocket.Message.Send(conn, fmt.Sprintf("%s", message))
+		// }
+	}
 }
-
